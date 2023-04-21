c39968d419e52b0dfb7c40e1ad70c0041634d849
docktermj <michael@dockter.com>
2023-04-21 16:32:39 -0400

    #57 Prepare for versioned release

--- cmd/github.go
+++ cmd/github.go
@@ -2,11 +2,10 @@
 // Created by make-go-github-file.yaml on Tue Apr 18 14:22:06 UTC 2023
 package cmd
 
-var githubDate            string = "2023-04-18"
-var githubIteration       string = "0"
-var githubRef             string = "refs/tags/0.2.2"
-var githubRefName         string = "0.2.2"
-var githubRepository      string = "Senzing/init-database"
-var githubRepositoryName  string = "init-database"
-var githubVersion         string = "0.2.2"
-
+var githubDate string = "2023-04-21"
+var githubIteration string = "0"
+var githubRef string = "refs/tags/0.2.2"
+var githubRefName string = "0.2.2"
+var githubRepository string = "Senzing/init-database"
+var githubRepositoryName string = "init-database"
+var githubVersion string = "0.2.2"
