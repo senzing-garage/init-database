0128fa19ee66cd6c2457601eee279555e62b9170
Sam <109683132+kernelsam@users.noreply.github.com>
2023-07-11 09:09:11 -0700

    Senzing/build-resources#27 fix go formatting

--- cmd/github.go
+++ cmd/github.go
@@ -2,11 +2,10 @@
 // Created by make-go-github-file.yaml on Sun Jun 18 19:44:32 UTC 2023
 package cmd
 
-var githubDate            string = "2023-06-18"
-var githubIteration       string = "0"
-var githubRef             string = "refs/tags/0.2.7"
-var githubRefName         string = "0.2.7"
-var githubRepository      string = "Senzing/init-database"
-var githubRepositoryName  string = "init-database"
-var githubVersion         string = "0.2.7"
-
+var githubDate string = "2023-06-18"
+var githubIteration string = "0"
+var githubRef string = "refs/tags/0.2.7"
+var githubRefName string = "0.2.7"
+var githubRepository string = "Senzing/init-database"
+var githubRepositoryName string = "init-database"
+var githubVersion string = "0.2.7"
