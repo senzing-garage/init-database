f56d9b04550652d19375757077dd9f07e02d3980
docktermj <docktermj@users.noreply.github.com>
2023-11-02 22:32:54 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.4.3

--- cmd/github.go
+++ cmd/github.go
@@ -1,15 +1,15 @@
-// 0.4.2
-// Created by make-go-github-file.yaml on Wed Oct 25 19:33:30 UTC 2023
+// 0.4.3
+// Created by make-go-github-file.yaml on Thu Nov  2 22:32:54 UTC 2023
 //
 //lint:file-ignore U1000 Ignore all unused code, it's generated
 package cmd
 
 var (
-	githubDate           string = "2023-10-25"
+	githubDate           string = "2023-11-02"
 	githubIteration      string = "0"
-	githubRef            string = "refs/tags/0.4.2"
-	githubRefName        string = "0.4.2"
+	githubRef            string = "refs/tags/0.4.3"
+	githubRefName        string = "0.4.3"
 	githubRepository     string = "Senzing/init-database"
 	githubRepositoryName string = "init-database"
-	githubVersion        string = "0.4.2"
+	githubVersion        string = "0.4.3"
 )
