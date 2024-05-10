a22c934ab7fb7331eb17226e2ca162a5a83223b7
docktermj <michael@dockter.com>
2024-05-10 10:49:59 -0400

    Update format

--- cmd/github.go
+++ cmd/github.go
@@ -1,18 +1,3 @@
-// 0.5.2
-// Created by make-go-github-file.yaml on Tue Jan 30 16:23:47 UTC 2024
-//
-//lint:file-ignore U1000 Ignore all unused code, it's generated
-package cmd
-
-var (
-	githubDate           string = "2024-03-19"
-	githubIteration      string = "0"
-	githubRef            string = "refs/tags/0.5.2"
-	githubRefName        string = "0.5.2"
-	githubRepository     string = "senzing-garage/init-database"
-	githubRepositoryName string = "init-database"
-	githubVersion        string = "0.5.2"
-)
 // 0.5.3
 // Created by make-go-github-file.yaml on Tue Mar 19 15:42:28 UTC 2024
 //
