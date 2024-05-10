ac61996d3505a2674ea81687f9fa141b5aeeaf60
senzingdevops <senzingdevops@users.noreply.github.com>
2024-05-10 14:56:59 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.6.1

--- cmd/github.go
+++ cmd/github.go
@@ -1,15 +1,15 @@
-// 0.5.3
-// Created by make-go-github-file.yaml on Tue Mar 19 15:42:28 UTC 2024
+// 0.6.1
+// Created by make-go-github-file.yaml on Fri May 10 14:56:59 UTC 2024
 //
 //lint:file-ignore U1000 Ignore all unused code, it's generated
 package cmd
 
 var (
-	githubDate           string = "2024-03-19"
+	githubDate           string = "2024-05-10"
 	githubIteration      string = "0"
-	githubRef            string = "refs/tags/0.5.3"
-	githubRefName        string = "0.5.3"
+	githubRef            string = "refs/tags/0.6.1"
+	githubRefName        string = "0.6.1"
 	githubRepository     string = "senzing-garage/init-database"
 	githubRepositoryName string = "init-database"
-	githubVersion        string = "0.5.3"
+	githubVersion        string = "0.6.1"
 )
