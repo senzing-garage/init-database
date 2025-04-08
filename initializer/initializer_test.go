2e20313a87c805a6b25f55d10558dbf8faf2ea05
docktermj <michael@dockter.com>
2025-04-08 14:23:37 -0400

    #356 Savepoint

--- initializer/initializer_test.go
+++ initializer/initializer_test.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"testing"
 
+	"github.com/senzing-garage/go-helpers/env"
 	"github.com/senzing-garage/go-helpers/settings"
 	"github.com/senzing-garage/go-logging/logging"
 	"github.com/senzing-garage/go-observing/observer"
-	"github.com/senzing-garage/sz-sdk-go-core/helper"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,7 +16,7 @@ const (
 )
 
 var (
-	logLevel          = helper.GetEnv("SENZING_LOG_LEVEL", "INFO")
+	logLevel          = env.GetEnv("SENZING_LOG_LEVEL", "INFO")
 	observerSingleton = &observer.NullObserver{
 		ID:       "Observer 1",
 		IsSilent: true,
